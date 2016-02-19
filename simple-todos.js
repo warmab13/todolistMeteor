240426f3b8be131ccfcc1e9ccb4834325c671203
Alonso S <alonsosalcido13@gmail.com>
2016-02-19 12:28:55 -0700

    Todo List Code

---
+++ simple-todos.js
@@ -0,0 +1,45 @@
+Tasks = new Mongo.Collection("tasks");
+
+if(Meteor.isClient){
+	Template.body.helpers({
+		tasks: function(){
+			if(Session.get("hideCompleted")){
+				return Tasks.find({checked: {$ne: true}}, {sort:{createdAt: -1}});
+			}else{
+				return Tasks.find({}, {sort:{createdAt: -1}});
+			}
+		},
+		hideCompleted: function(){
+			return Session.get("hideCompleted");
+		}
+	});
+
+	Template.body.events({
+		"submit .new-task": function(event){
+			event.preventDefault();
+
+			var text = event.target.text.value;
+
+			Tasks.insert({
+				text: text,
+				createdAt: new Date()
+			});
+
+			event.target.text.value = "";
+		},
+		"change .hide-completed input": function(event){
+			Session.set("hideCompleted", event.target.checked);
+		}
+	});
+
+	Template.task.events({
+		"click .toggle-checked": function(){
+			Tasks.update(this._id, {
+				$set: {checked: ! this.checked}
+			});
+		},
+		"click .delete": function(){
+			Tasks.remove(this._id);
+		}
+	});
+}
